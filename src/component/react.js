66e2e5dd8a8f39a6009b05cec74a093d08cf5b36
gunjan-12 <gunjanshrivastava175@gmail.com>
2021-07-07 15:05:09 +0530

    updated

---
+++ src/component/react.js
@@ -0,0 +1,18 @@
+
+
+function Welcome() {
+  return (
+    
+    <div id ="root">
+        <p>
+        "I am learning React. My life is getting better. "
+
+        </p>
+    </div>
+    
+          
+  
+  );
+}
+
+export default Welcome;
